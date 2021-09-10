adf5489beec5037e2b2b23f01032c18dd20389ac
AleksandrZaprudnov <88497549+AleksandrZaprudnov@users.noreply.github.com>
2021-09-10 22:54:12 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Configuration the program
+
+TOKETN = '1912537115:AAEnw6rNKCB_0JSPLclm4UqQvPwKH1EGytY'
+API_KEY = 'a8022da27c6dfc3bbfa754f8b8218c16'
+
+keys = {
+    'Биткоин': 'BTC',
+    'Доллар': 'USD',
+    'Рубль': 'RUB',
+    'Эфириум': 'ETH'
+}
